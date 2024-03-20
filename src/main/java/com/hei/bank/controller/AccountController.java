8e70a4aaeebcdfe3bfbefc15be773412259bbaf9
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-03-20 21:06:30 +0100

    feat : Create configuration cors

--- src/main/java/com/hei/bank/controller/AccountController.java
+++ src/main/java/com/hei/bank/controller/AccountController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class AccountController {
     private final AccountService accountService;
 
